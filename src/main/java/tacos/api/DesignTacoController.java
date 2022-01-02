3636ac3fb2a6848aab3bc054a76b5c6491d9d4f0
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-02 23:19:29 +0900

    CH06. @RequiredArgsConstructor, @CrossOrigin 누락 추가

--- src/main/java/tacos/api/DesignTacoController.java
+++ src/main/java/tacos/api/DesignTacoController.java
@@ -1,17 +1,20 @@
 package tacos.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import tacos.Taco;
 import tacos.data.TacoRepository;
 
 @RestController
-@RequestMapping(path = "/design", produces = "application/json") // 요청 Accept 헤더에 application/json 요청만 처리 
+@RequestMapping(path = "/design", produces = "application/json") // 요청 Accept 헤더에 application/json 요청만 처리
+@CrossOrigin(origins = "*") // cors 이슈 해결하기 위해 사용
+@RequiredArgsConstructor
 public class DesignTacoController {
 	
 	private final TacoRepository tacoRepo;
