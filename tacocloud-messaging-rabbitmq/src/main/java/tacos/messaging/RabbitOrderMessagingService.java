bc012fa700933eb7e9723dee7857b8561ab45d6c
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-27 23:56:59 +0900

    CH08. RabbitTemplate.send()로 메세지 전송하기 [p.280]

--- tacocloud-messaging-rabbitmq/src/main/java/tacos/messaging/RabbitOrderMessagingService.java
+++ tacocloud-messaging-rabbitmq/src/main/java/tacos/messaging/RabbitOrderMessagingService.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,16 +23,11 @@ public class RabbitOrderMessagingService
   }
   
   public void sendOrder(Order order) {
-    rabbit.convertAndSend("tacocloud.order.queue", order,
-        new MessagePostProcessor() {
-          @Override
-          public Message postProcessMessage(Message message)
-              throws AmqpException {
-            MessageProperties props = message.getMessageProperties();
-            props.setHeader("X_ORDER_SOURCE", "WEB");
-            return message;
-          } 
-        });
+      MessageConverter converter = rabbit.getMessageConverter();
+      MessageProperties props = new MessageProperties();
+      // MessageConverter 로 Order 객체를 Message 객체로 변환하여 send()로 전송
+      Message message = converter.toMessage(order, props);
+      // 라우팅 키 => tacocloud.order [기본 거래소 사용]
+      rabbit.send("tacocloud.order", message);
   }
-  
 }
