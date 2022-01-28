5528b9bd8d4378134983689e0ad355c8dcd21a8f
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-28 22:19:13 +0900

    CH08. MessageProperties를 통한 메세지 속성 설정  [p.282]

--- tacocloud-messaging-rabbitmq/src/main/java/tacos/messaging/RabbitOrderMessagingService.java
+++ tacocloud-messaging-rabbitmq/src/main/java/tacos/messaging/RabbitOrderMessagingService.java
@@ -5,8 +5,10 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import tacos.Order;
@@ -14,7 +16,12 @@ import tacos.Order;
 @Service
 public class RabbitOrderMessagingService
        implements OrderMessagingService {
-  
+
+  @Bean
+  public MessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
   private RabbitTemplate rabbit;
   
   @Autowired
@@ -23,7 +30,11 @@ public class RabbitOrderMessagingService
   }
   
   public void sendOrder(Order order) {
-    // 객체로부터 전환된 메세지를 전송한다.
-    rabbit.convertAndSend("tacocloud.order", order);
+    MessageConverter converter = rabbit.getMessageConverter();
+    MessageProperties props = new MessageProperties();
+    // JMS처럼 MessageProperties 인스턴스를 통해 헤더를 설정 할 수 있다
+    props.setHeader("X_ORDER_SOURCE", "WEB");
+    Message message = converter.toMessage(order, props);
+    rabbit.send("tacocloud.order", message);
   }
 }
