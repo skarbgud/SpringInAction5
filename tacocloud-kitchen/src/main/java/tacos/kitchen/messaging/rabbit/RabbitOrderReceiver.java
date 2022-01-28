2eda4245271d5f2fae23620f4928ad5ffe840499
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-28 23:15:20 +0900

    CH08. receiveAndConvert() 메서드로 데이터 수신[p.286]

--- tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
+++ tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
@@ -13,18 +13,14 @@ import tacos.kitchen.OrderReceiver;
 public class RabbitOrderReceiver {
 
   private RabbitTemplate rabbit;
-  private MessageConverter converter;
 
   @Autowired
   public RabbitOrderReceiver(RabbitTemplate rabbit) {
     this.rabbit = rabbit;
-    this.converter = rabbit.getMessageConverter();
   }
   
   public Order receiveOrder() {
-    // timeoutMillis 에 대한 값을 인자로 전달하지 않았으므로 바로 Message 객체 또는 null 값 반환 => 두번째 인자에 값을 넣으면 해당 시간동안 기다린다(ex> 30000밀리초(30초) 대기)
-    Message message = rabbit.receive("tacocloud.orders", 30000);
-    // 메세지가 반환이 되면 Order 객체로 캐스팅 : null 일 경우 null 값 반환
-    return message != null ? (Order) converter.fromMessage(message) : null;
+    // receiveAndConverter()를 사용하여 더 간단하게 작성 할 수 있다.
+    return (Order) rabbit.receiveAndConvert("tacocloud.order.queue");
   }
 }
