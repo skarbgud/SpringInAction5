7343a27ce613022f147d242f7bf15ec45943a6a8
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-28 22:37:19 +0900

    CH08. 메세지 수신 timeout 설정[p.285 하단]

--- tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
+++ tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
@@ -22,8 +22,8 @@ public class RabbitOrderReceiver {
   }
   
   public Order receiveOrder() {
-    // timeoutMillis 에 대한 값을 인자로 전달하지 않았으므로 바로 Message 객체 또는 null 값 반환
-    Message message = rabbit.receive("tacocloud.orders");
+    // timeoutMillis 에 대한 값을 인자로 전달하지 않았으므로 바로 Message 객체 또는 null 값 반환 => 두번째 인자에 값을 넣으면 해당 시간동안 기다린다(ex> 30000밀리초(30초) 대기)
+    Message message = rabbit.receive("tacocloud.orders", 30000);
     // 메세지가 반환이 되면 Order 객체로 캐스팅 : null 일 경우 null 값 반환
     return message != null ? (Order) converter.fromMessage(message) : null;
   }
