5d59a6875cb66ef5fda91f2cd99de411a45e6ce4
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-28 22:35:20 +0900

    CH08. RabbitTemplate를 사용해 RabbitMQ로 부터 메세지의 데이터 수신하기[p.285]

--- tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
+++ tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
@@ -1,24 +1,30 @@
 package tacos.kitchen.messaging.rabbit;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.context.annotation.Profile;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import tacos.Order;
 import tacos.kitchen.OrderReceiver;
 
-@Profile("rabbitmq-template")
-@Component("templateOrderReceiver")
-public class RabbitOrderReceiver implements OrderReceiver {
+@Component
+public class RabbitOrderReceiver {
 
   private RabbitTemplate rabbit;
+  private MessageConverter converter;
 
+  @Autowired
   public RabbitOrderReceiver(RabbitTemplate rabbit) {
     this.rabbit = rabbit;
+    this.converter = rabbit.getMessageConverter();
   }
   
   public Order receiveOrder() {
-    return (Order) rabbit.receiveAndConvert("tacocloud.order.queue");
+    // timeoutMillis 에 대한 값을 인자로 전달하지 않았으므로 바로 Message 객체 또는 null 값 반환
+    Message message = rabbit.receive("tacocloud.orders");
+    // 메세지가 반환이 되면 Order 객체로 캐스팅 : null 일 경우 null 값 반환
+    return message != null ? (Order) converter.fromMessage(message) : null;
   }
-  
 }
