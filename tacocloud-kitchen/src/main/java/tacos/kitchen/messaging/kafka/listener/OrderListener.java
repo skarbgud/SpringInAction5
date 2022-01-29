333ba6dd7050241cd5e6bd65fd2b35d100811624
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-30 01:16:56 +0900

    CH08. Message 객체를 통한 메세지 수신

--- tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/kafka/listener/OrderListener.java
+++ tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/kafka/listener/OrderListener.java
@@ -1,15 +1,19 @@
 package tacos.kitchen.messaging.kafka.listener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 import tacos.Order;
 import tacos.kitchen.KitchenUI;
 
-
+@Slf4j
 @Component
 public class OrderListener {
-  
+
   private KitchenUI ui;
 
   @Autowired
@@ -19,7 +23,11 @@ public class OrderListener {
 
   // tacocloud.orders.topic 이름의 토픽에 메세지가 도착할 때 까지 자동호출되어야 한다는 것을 나타내기 위해 @KafkaListener 지정
   @KafkaListener(topics="tacocloud.orders.topic")
-  public void handle(Order order) { // 페이로드인 Order 만 인자로 받는다
+  public void handle(Order order, Message<Order> message) {
+    MessageHeaders headers = message.getHeaders();
+    log.info("Received from partition {} with timestamp {}",
+        headers.get(KafkaHeaders.RECEIVED_PARTITION_ID),
+        headers.get(KafkaHeaders.RECEIVED_TIMESTAMP));
     ui.displayOrder(order);
   }
 }
